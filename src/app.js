caf7e62efcd42ab4921ac228de25750bc6ac7ca4
Kaique Pc senai <kaique>
2024-11-29 15:26:17 -0300

    configurando cors e habilitando secure true em cookies

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const imoveis = require('./routes/DTOimoveis')
 const app = express();
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     credentials: true
 }))
 app.use(express.json());
--- src/services/jwtToken.js
+++ src/services/jwtToken.js
@@ -11,7 +11,7 @@ const gerarToken = (user, res) => {
   // Salvar o token no cookie
   res.cookie('token', token, {
     httpOnly: true,  // O cookie não será acessível via JavaScript no navegador
-    secure: false, // Apenas em dev
+    secure: true, // Apenas em dev
     maxAge: 60 * 60 * 1000, // Expira em 5 minutos (em milissegundos)
     // posteriormente mudar para: 60 * 60 * 1000 (1h)
   });
