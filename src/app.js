ef3136fddb280e3482beab0ad9d1913d55deaae1
Kaique Pc senai <kaique>
2024-11-29 15:02:50 -0300

    habilitando cors com origin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ const imoveis = require('./routes/DTOimoveis')
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
