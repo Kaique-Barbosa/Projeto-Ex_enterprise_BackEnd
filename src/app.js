17c4bac74c7d9a8aaff41ae80e77a8c1bcad2b4a
Kaique Barbosa <kaiquebarbosa2103@gmail.com>
2024-12-06 23:12:35 -0300

    tirei a barra

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const imoveis = require('./routes/DTOimoveis')
 const app = express();
 
 app.use(cors({
-    origin: "https://ex-enterprise.vercel.app/",
+    origin: "https://ex-enterprise.vercel.app",
     credentials: true
 }))
 app.use(express.json());
