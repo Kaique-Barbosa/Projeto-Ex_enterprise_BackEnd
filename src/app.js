9817fa8fd8eddd0d956ff92209987771c874da25
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-12-07 10:01:24 -0300

    ajustando configuração do CORS para permitir múltiplas origens

--- src/app.js
+++ src/app.js
@@ -1,30 +1,31 @@
 // src/app.js
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors= require('cors')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 // inportando rotas
-const usuario = require('./routes/DTOUsuarios')
-const verificarToken = require('./routes/verificarToken')
-const gerarContrato = require('./routes/gerarContrato')
-const imoveis = require('./routes/DTOimoveis')
+const usuario = require("./routes/DTOUsuarios");
+const verificarToken = require("./routes/verificarToken");
+const gerarContrato = require("./routes/gerarContrato");
+const imoveis = require("./routes/DTOimoveis");
 
 const app = express();
 
-app.use(cors({
-    origin: "https://ex-enterprise.vercel.app" ||"http://localhost:3000",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ["https://ex-enterprise.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware para parsing de cookies
 app.use(cookieParser()); // necessario para manipular os tokens, foi instalado
 
-
 //Inportação do Fluxo de rotas:
-app.use("/usuario", usuario)
-app.use("/imoveis", imoveis)
-app.use("/verificarToken", verificarToken) // rota para verificar token no front
-app.use("/pdf", gerarContrato) // rota paragerar o contrato preenchido
+app.use("/usuario", usuario);
+app.use("/imoveis", imoveis);
+app.use("/verificarToken", verificarToken); // rota para verificar token no front
+app.use("/pdf", gerarContrato); // rota paragerar o contrato preenchido
 
 module.exports = app;
