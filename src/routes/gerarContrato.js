ddffb3561f763d038f82905f29aa945bc0c2f70e
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-12-11 12:07:31 -0300

    Adicionando dados restantes para geração de contrato

--- src/routes/gerarContrato.js
+++ src/routes/gerarContrato.js
@@ -1,20 +1,55 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 const verificarRota = require("../middleware/VerificarRotaProtegida");
-const { generatePdf } = require("../middleware/gerarPdf"); 
+const { generatePdf } = require("../middleware/gerarPdf");
 const rotaPdf = express.Router();
+const prisma = new PrismaClient();
 
 // adicionar depois o verificarRota abaixo
 rotaPdf.post("/gerar", async (req, res) => {
-  const dadosLocador = req.body;
+  const { imovelCod, ...dadosLocador } = req.body;
+
+  const imovel = await prisma.imovel.findFirst({
+    where: { codigo: imovelCod },
+  });
 
   // Validação simples dos dados (ajuste conforme necessário)
   if (!dadosLocador || !dadosLocador.nomeLocador) {
-    return res.status(400).json({ error: "Dados inválidos, nome do locador é obrigatório." });
+    return res
+      .status(400)
+      .json({ error: "Dados inválidos, nome do locador é obrigatório." });
   }
+
+  if (!imovel) {
+    return res.status(404).json({ error: "Imóvel não encontrado." });
+  }
+
+  const enderecoImovel = imovel.endereco.split(", ");
+
+  const dadosParaContrato = {
+    ...dadosLocador,
+    logradouroImovel: enderecoImovel[0],
+    numeroImovel: enderecoImovel[1],
+    bairroImovel: enderecoImovel[2],
+    cidadeImovel: enderecoImovel[3],
+    prazoLocacao: "12",
+    valorMulta: "500,00",
+    valorMultaExtenso: "quinhentos reais",
+    valorLocacao: imovel.valorAlocacao + ",00",
+    valorLocacaoExtenso: "três mil reais",
+    diaPagamentoLocacao: "5",
+    porcentagemMulta: "2",
+    porcentagemMultaExtenso: "dois por cento",
+    jurosMora: "1",
+    jurosMoraExtenso: "um por cento",
+    limiteDiasAtrasoPagamento: "5",
+    prazoServico: "12",
+    servicos: ["Pintura", " Encanamento", " Elétrica", " Limpeza"],
+  };
 
   try {
     // Gera o PDF e realiza o upload no Vercel Blob
-    const {url} = await generatePdf(dadosLocador);
+    const { url } = await generatePdf(dadosParaContrato);
 
     // Retorna a URL do arquivo salvo no Vercel Blob
     res.status(200).json({
